6186a2453b3a594b7d5d050abda69265009f0161
余静 <yu1441@qq.com>
2022-07-04 16:16:54 +0800

    批量更新（替换）文本文件中的指定字符串

--- src/com/yujing/Main.java
+++ src/com/yujing/Main.java
@@ -13,11 +13,11 @@ import java.io.File;
 打包jar：
   1.项目右键 ——> 构建模块
   2.文件菜单 ——> 项目结构 ——> 点击构建（工作）——> 点击+ ——> 选择jar ——> 选择来自具有依赖项的模块 ——> 选择main函数，选择提取到目标jar --> 点击应用
-  3.菜单构建 ——> 构建模块'replace'
+  3.菜单构建 ——> 构建工件... ——> 构建
   4.生成的jar目录：build\artifacts\replace_jar\***.jar
 
 使用方法：
-call java -jar -Dfile.encoding=utf-8 replace.jar
+call java -jar -Dfile.encoding=utf-8 replace.jar json文件
  */
 public class Main {
   public static void main(String... args) {
